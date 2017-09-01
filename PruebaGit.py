330c28b814e19240739a591d184fc2b587657d5e
Chinaski <jonathan.acunah@utem.cl>
2017-09-01 15:53:06 -0300

    Cuadrado

--- PruebaGit.py
+++ PruebaGit.py
@@ -1,3 +1,9 @@
+def cuadrado(n):
+    return n*n
+
 print "hola Mundo"
 sum = 2+2
 print 'sa',sum, 'asda', 2+2
+
+num = int(input('Ingresa un numero: '))
+print 'Tu numero al cuadrado es', cuadrado(num)
