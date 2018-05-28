9f92fa68bd17397de2d6bc420abd1876b3c6e16f
chinoxpito <chinoxpito@hotmail.com>
2018-05-27 23:11:12 -0400

    Audio

---
+++ Audio/Audio.py
@@ -0,0 +1,11 @@
+import os
+import numpy as np
+#import matplotlib.pyplot as plt
+#import scipy.io.wavfile as waves
+
+
+# archivo = input('archivo de sonido:' )
+archivo = '3-95695-B-5.wav'
+muestreo, sonido = waves.read(archivo)
+
+print ("holaMundo")
